ab0d0ac4f0cd005ef0bdd954cc865d54ddb8cb69
Manisha Pradip ghuge <manishaghuge2008@gmail.com>
2026-06-28 22:14:24 +0530

    Refactor analyze_string function for clarity
    Removed unnecessary comments and added whitespace for readability.

--- analyze_string.py
+++ analyze_string.py
@@ -1,16 +1,16 @@
 def analyze_string(s):
-    # Check empty string
+    
     if s == "":
         print("String is empty!")
         return
 
-    # Length of string
+    
     print("Length of string:", len(s))
 
-    # Reverse string
+    
     print("Reversed string:", s[::-1])
 
-    # Count vowels
+    
     vowels = "aeiouAEIOU"
     count = 0
     for ch in s:
@@ -18,14 +18,14 @@ def analyze_string(s):
             count += 1
     print("Number of vowels:", count)
 
-    # Print each character with index
+    
     print("Characters with index:")
     for i in range(len(s)):
         print("Index", i, ":", s[i], " | Negative Index:", i - len(s))
 
 
-# Take user input
+
 user_input = input("Enter a string: ")
 
-# Call function
+
 analyze_string(user_input)
